cca3f13f4607733401101eaec7cf93fc2924d688
Pavol Juhas <pavol.juhas@gmail.com>
2018-10-20 22:19:28 -0400

    Propagate environment variable CXX to scons

--- recipe/build.sh
+++ recipe/build.sh
@@ -3,5 +3,8 @@
 export CPATH="$PREFIX/include:$CPATH"
 MYNCPU=$(( (CPU_COUNT > 8) ? 8 : CPU_COUNT ))
 
+# apply customizations in sconscript.local
+cp ${RECIPE_DIR}/sconscript.local ./
+
 scons -j $MYNCPU
 scons install prefix=$PREFIX
