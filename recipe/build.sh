eb746e5f03cfd10f26e021d3156b3f5987e9ee2d
st3107 <st3107@columbia.edu>
2021-06-07 09:55:01 -0400

    BUG: rename the sconscript.local to SConstruct to debug "the scon file cannot be found"

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ export CPATH="$PREFIX/include:$CPATH"
 MYNCPU=$(( (CPU_COUNT > 8) ? 8 : CPU_COUNT ))
 
 # apply customizations in sconscript.local
-cp ${RECIPE_DIR}/sconscript.local ./
+cp ${RECIPE_DIR}/SConstruct ./
 
 scons -j $MYNCPU
 scons install prefix=$PREFIX
