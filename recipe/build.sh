16874b61ea6e9a0bf0a762c718cb1f1007dcc142
st3107 <st3107@columbia.edu>
2021-06-07 11:58:45 -0400

    BUG: update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,4 +6,9 @@ MYNCPU=$(( (CPU_COUNT > 8) ? 8 : CPU_COUNT ))
 # apply customizations in sconscript.local
 cp ${RECIPE_DIR}/SConstruct ./
 
+echo "Here is the directory before build"
+tree .
+
 scons -j $MYNCPU
+
+echo "Here is the directory after build"
