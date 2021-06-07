d8f05ecc3863aaebaaddaaf3bdf683672c2ff80f
st3107 <st3107@columbia.edu>
2021-06-07 14:01:27 -0400

    BUG: update recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,14 +4,14 @@ export CPATH="$PREFIX/include:$CPATH"
 MYNCPU=$(( (CPU_COUNT > 8) ? 8 : CPU_COUNT ))
 
 # apply customizations in sconscript.local
-cp ${RECIPE_DIR}/SConstruct ./
+cp ${RECIPE_DIR}/SConstruct $SRC_DIR/
 
 echo "Here is the directory before build"
 ls $SRC_DIR
 echo "Here is what in the src directory before build"
 ls $SRC_DIR/src/ObjCryst
 
-scons -j $MYNCPU
+scons -j $MYNCPU -C $SRC_DIR
 
 echo "Here is the directory after build"
 ls $SRC_DIR
