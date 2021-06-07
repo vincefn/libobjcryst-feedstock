39caf555369061832c518d57626e38d26ea837cf
st3107 <st3107@columbia.edu>
2021-06-07 15:35:08 -0400

    Revert "BUG: update build.sh"
    This reverts commit 8acce308

--- recipe/build.sh
+++ recipe/build.sh
@@ -5,12 +5,6 @@ cd $SRC_DIR
 export CPATH="$PREFIX/include:$CPATH"
 MYNCPU=$(( (CPU_COUNT > 8) ? 8 : CPU_COUNT ))
 
-# point to the correct compilers
-export CFLAGS="${CFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
-export CPPFLAGS="${CPPFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
-export CXXFLAGS="${CXXFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
-export LDFLAGS="${LDFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
-
 # apply customizations in sconscript.local
 cp ${RECIPE_DIR}/sconscript.local ./
 
