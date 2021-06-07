ff6a3d69666d3f624a39c36baaf51c155fe8bd62
st3107 <st3107@columbia.edu>
2021-06-07 17:26:13 -0400

    BUG: update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,15 @@ echo "build_platform: ${build_platform}"
 export CPATH="$PREFIX/include:$CPATH"
 MYNCPU=$(( (CPU_COUNT > 8) ? 8 : CPU_COUNT ))
 
+# use macos SDK
+if [ $build_platform = "osx-64" ]
+then
+  echo "Use conda compiler."
+  export CFLAGS="${CFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
+  export CPPFLAGS="${CPPFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
+  export CXXFLAGS="${CXXFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
+fi
+
 # apply customizations in sconscript.local
 cp ${RECIPE_DIR}/sconscript.local ./
 
