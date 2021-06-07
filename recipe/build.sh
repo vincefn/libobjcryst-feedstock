001e144b2ab327ffa3639d7e7bddfce4214001cc
st3107 <st3107@columbia.edu>
2021-06-07 14:29:07 -0400

    BUG: update recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,19 +1,21 @@
 #!/bin/bash
 
+cd $SRC_DIR
+
 export CPATH="$PREFIX/include:$CPATH"
 MYNCPU=$(( (CPU_COUNT > 8) ? 8 : CPU_COUNT ))
 
 # apply customizations in sconscript.local
-cp ${RECIPE_DIR}/SConstruct $SRC_DIR/src
+cp ${RECIPE_DIR}/SConscript.local ./
 
 echo "Here is the directory before build"
-ls $SRC_DIR
+ls .
 echo "Here is what in the src directory before build"
-ls $SRC_DIR/src
+ls ./src
 
-scons -j $MYNCPU -C $SRC_DIR/src
+scons -j $MYNCPU
 
 echo "Here is the directory after build"
-ls $SRC_DIR
+ls .
 echo "Here is what in the src directory after build"
-ls $SRC_DIR/src
+ls ./src
