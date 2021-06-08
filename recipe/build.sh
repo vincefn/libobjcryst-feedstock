0927c3c213fecbc29bf3cbaf8fcd6361b1112c64
st3107 <st3107@columbia.edu>
2021-06-07 21:43:35 -0400

    BUG: update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,11 @@ MYNCPU=$(( (CPU_COUNT > 8) ? 8 : CPU_COUNT ))
 # use macos SDK
 if [ $build_platform = "osx-64" ]
 then
-  echo "Use conda compiler."
+  echo "Download SDK"
+  curl -L -O https://github.com/phracker/MacOSX-SDKs/releases/download/10.13/MacOSX10.9.sdk.tar.xz
+  tar -xvf MacOSX10.9.sdk.tar.xz -c /opt/
+  rm MacOSX10.9.sdk.tar.xz
+  echo "Use SDK."
   export CXXFLAGS="${CXXFLAGS} -isysroot ${CONDA_BUILD_SYSROOT}"
 fi
 
