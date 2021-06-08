0a3cb901a731afb5906714b826f69e1bf25984bd
st3107 <st3107@columbia.edu>
2021-06-07 21:08:20 -0400

    BUG: update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -10,11 +10,11 @@ MYNCPU=$(( (CPU_COUNT > 8) ? 8 : CPU_COUNT ))
 if [ $build_platform = "osx-64" ]
 then
   echo "Use conda compiler."
-  export CFLAGS="${CFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
-  export CCFLAGS="${CCFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
-  export CPPFLAGS="${CPPFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
-  export CXXFLAGS="${CXXFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
-  export LINKFLAGS="${LINKFLAGS} -i sysroot ${CONDA_BUILD_SYSROOT}"
+  export CFLAGS="-i sysroot ${CONDA_BUILD_SYSROOT}"
+  export CCFLAGS="-i sysroot ${CONDA_BUILD_SYSROOT}"
+  export CPPFLAGS="-i sysroot ${CONDA_BUILD_SYSROOT}"
+  export CXXFLAGS="-i sysroot ${CONDA_BUILD_SYSROOT}"
+  export LINKFLAGS="-i sysroot ${CONDA_BUILD_SYSROOT}"
 fi
 
 # apply customizations in sconscript.local
